886674996aad47fa8cc9d9edac5af65c0524c24b
virgotatus <435491723@qq.com>
2023-09-27 18:06:20 +0800

    Nav Bar style

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,27 +3,31 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div>
-      <aside className="bg-gray-100 dark:bg-gray-900 border mb-3 space-x-3">
-        <div className="join">
-          <Link className="join-item btn" href="/">
+    <div className="mb-10">
+      <aside className="navbar bg-gray-100 dark:bg-gray-900 border space-x-3">
+        <div className="flex-1">
+          <Link className="text-lg font-bold" href="/">
             龚一隆
           </Link>
-          <Link className="join-item btn" href="/projects">
-            作品
-          </Link>
-          <Link className="join-item btn" href="/counseling">
-            心理咨询
-          </Link>
-          <Link className="join-item btn" href="/articles">
-            写作
-          </Link>
-          <Link className="join-item btn" href="/parties">
-            活动
-          </Link>
-          <Link className="join-item btn" href="/about">
-            关于
-          </Link>
+        </div>
+        <div className="flex-none">
+          <div className="menu menu-horizontal px-1 join">
+            <Link className="join-item btn" href="/projects">
+              作品
+            </Link>
+            <Link className="join-item btn" href="/counseling">
+              心理咨询
+            </Link>
+            <Link className="join-item btn" href="/articles">
+              写作
+            </Link>
+            <Link className="join-item btn" href="/parties">
+              活动
+            </Link>
+            <Link className="join-item btn" href="/about">
+              关于
+            </Link>
+          </div>
         </div>
       </aside>
     </div>
