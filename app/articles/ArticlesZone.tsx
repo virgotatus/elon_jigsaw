8c5e41f19aff82f1344bc6b1fd8b65250183ebff
virgotatus <435491723@qq.com>
2023-10-13 17:55:35 +0800

    fix baseurl

--- app/articles/ArticlesZone.tsx
+++ app/articles/ArticlesZone.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ArticleCard, { MetaTag } from "./ArticleCard";
 
 const ArticlesZone = async () => {
-  const response = await fetch(process.env.BASE_URL + "/api/wechat/");
+  const response = await fetch(process.env.BASE_URL + "/api/wechat");
   const articles: MetaTag[] = await response.json();
   return (
     <div className="grid grid-cols-3 gap-3 space-y-3">
